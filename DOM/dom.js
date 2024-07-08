5864809716c383d7779f83a2d2dc92fc751822ea
devjahid1 <developerjahid1@gmail.com>
2024-07-08 07:31:49 +0600

    third-committ

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,18 +1,68 @@
+function play(){
+    hiddenElementById('home-screen')
+    ShowElementById('play-ground')
+    setTextElementById('current-life', 5)
+    setTextElementById('current-score', 0)
+    continueGame();
+    
+}
+
+
+
+
 function continueGame(){
-    //Generate Random Alphabet
-    const alphabet = getARandomAlphabet();
-
-    // Set Random Alphabet to the screen
-    const currentAlphabet = document.getElementById('current-alphabet')
-    currentAlphabet.innerText = alphabet
-
-    //Set background color
+    const alphabet = getRandomAlphabet();
+    const currentAlphabet = document.getElementById('current-alphabet');
+    console.log(alphabet);
+    currentAlphabet.innerText = alphabet;
     setBackgroundColorById(alphabet)
 }
 
 
-function play(){
-    hiddenElementById('home-screen');
-    ShowElementById('play-ground');
-    continueGame();
-}
+document.addEventListener('keyup', function handleKeyboardButtonPress(event){
+    const playerPressed = event.key;
+    console.log(playerPressed);
+
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
+
+
+    const currentAlphabetElement =document.getElementById('current-alphabet') ;
+    const currentAlphabet = currentAlphabetElement.innerText;
+
+
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    if(playerPressed === expectedAlphabet){
+        console.log('you get a point');
+
+        const currentScore = getTextElementTextById('current-score');
+        const newScore = currentScore+1;
+        setTextElementById('current-score',newScore)
+
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('you missed');
+        const currentLife = getTextElementTextById('current-life');
+        const newLife = currentLife-1;
+        setTextElementById('current-life',newLife);
+        if(newLife === 0){
+            gameOver()
+        }
+    }
+
+})
+
+function gameOver(){
+
+        hiddenElementById('play-ground');
+        ShowElementById('final-score');
+        //update final score
+        const lastScore = this.getElementById('current-score');
+        setTextElementById('last-score', lastScore);
+
+        const currentAlphabet =getElementById('current-alphabet')
+        removeBackgroundColorById(currentAlphabet)
+    }
--- DOM/utility.js
+++ DOM/utility.js
@@ -7,7 +7,6 @@ function ShowElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
-
 function setBackgroundColorById(elementId){
     const element = document.getElementById(elementId)
     element.classList.add('bg-orange-400');
@@ -18,16 +17,31 @@ function removeBackgroundColorById(elementId){
     element.classList.remove('bg-orange-400');
 }
 
+function getRandomAlphabet(){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+    const alphabet = alphabets[index];
+    return alphabet
+}
 
-function getARandomAlphabet(){
-    //Get or create Alphabet array
-       const alphabetString = 'abcdefghijklmnopqrstuvwxyz'
-       const alphabets = alphabetString.split('');
-    
-       //get Random Alphabet
-       const randomNumber = Math.random()*25;
-       const index = Math.round(randomNumber);
-    
-       const alphabet = alphabets[index];
-       return alphabet;
-    }
+
+function getTextElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value
+}
+
+function setTextElementById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+
+function getElementById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
